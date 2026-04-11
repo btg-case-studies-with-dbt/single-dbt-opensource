65f6e31da2f7dad23137c7c2cfca42d2b30572f9
Kanja Saha <kanjasaha@gmail.com>
2026-04-10 17:56:28 -0700

    add customer and model availability snapshots

---
+++ dbt/snapshots/model_availability_snapshot.sql
@@ -0,0 +1,22 @@
+{% snapshot model_availability_snapshot %}
+{{
+    config(
+        target_schema='snapshots',
+        unique_key=['model_variant', 'source_region'],
+        strategy='check',
+        check_cols=['is_active'],
+        invalidate_hard_deletes=True
+    )
+}}
+
+select
+    model_variant,
+    source_region,
+    deployed_at,
+    is_active,
+    snapshot_date,
+    loaded_at,
+    source_file
+from {{ source('raw_bronze', 'config_model_region_availability') }}
+
+{% endsnapshot %}
